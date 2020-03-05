205dcaa562b0d85d50234d7997551cb664c0513a
clfang-8080 <1066166565@qq.com>
2020-03-05 10:09:56 +0800

    extract githubbeen for application.properties

--- src/main/java/com/clfang/community/community/controller/AuthorizeController.java
+++ src/main/java/com/clfang/community/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.clfang.community.community.dto.AccessTokenDto;
 import com.clfang.community.community.dto.GithubUser;
 import com.clfang.community.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,20 +19,28 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 public class AuthorizeController {
-    @Autowired
+    @Autowired//不用new对象，跳过创建
     private GithubProvider githubProvider;
+    @Value("${github.client.id}")//自动读取application.properties中的key，并赋值给下面的变量
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callbck(@RequestParam(name="code") String code,
                           @RequestParam(name="state") String state){
         AccessTokenDto token = new AccessTokenDto();
         token.setCode(code);
         token.setState(state);
-        token.setClient_id("Iv1.19c33fd30ca3617d");
-        token.setClient_secret("d1d04a66c18463af2bb775f444c99165dbc87db8");
-        token.setRedirect_uri("http://localhost:8887/callback");
+        token.setClient_id(clientId);
+        token.setClient_secret(clientSecret);
+        token.setRedirect_uri(redirectUri);
         String accessToken = githubProvider.getAccessToken(token);
         GithubUser user = githubProvider.getUser(accessToken);
         System.out.println(user.getName()+user.getId()+user.getBio());
+        System.out.println(clientId+">>"+clientSecret+">>"+redirectUri);
         return "index";
     }
 
